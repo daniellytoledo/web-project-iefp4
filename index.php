cbff6f74e455a36a5880e69dd84ddf22d7f1b44b
Dani <daniellytoledosouto@gmail.com>
2026-03-19 14:08:16 +0000

    divs

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Camalheia</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div>
+        navbar
+    </div>
+    <div>
+        <div> navbar de dentro</div>
+        conteúdo do meio da tala
+        <div> 4 caixinhas </div>
+    </div>
+    <footer>
+        O projeto Camalheia é exclusivamente patrocinado por Durex Lda, Harmony SA e Control Lda.
+    </footer>
+</body>
+</html>
