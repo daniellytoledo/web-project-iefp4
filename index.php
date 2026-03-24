58d3cdc15b8e500ba37c5c4e59625ae9570c8c09
Dani <daniellytoledosouto@gmail.com>
2026-03-24 10:37:03 +0000

    form

--- index.php
+++ index.php
@@ -7,16 +7,29 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div>
-        navbar
-    </div>
-    <div>
-        <div> navbar de dentro</div>
+    <header>
+        <div class="navbar_header">
+            <img src="imgs/imgs/logo.png" alt="logo" id="img_logo">
+        </div>
+        <p id="slogan">
+            Projeto Camalheia :: viaje pelo mundo ficando alojado na cama... alheia.
+        </p>
+        <form action="" name="form_login" method="POST" enctype="application/x-www-form-urlencoded">
+            <input type="text" name="Login">
+            <input type="password" name="pass">
+            <input type="submit" value="entrar">
+        </form>
+    </header>
+
+    <main>
+        <div id="navbar_pesquisa"> pesquisa </div>
         conteúdo do meio da tala
         <div> 4 caixinhas </div>
-    </div>
+    </main>
+
     <footer>
         O projeto Camalheia é exclusivamente patrocinado por Durex Lda, Harmony SA e Control Lda.
     </footer>
+
 </body>
 </html>
